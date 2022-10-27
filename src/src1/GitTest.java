e31ce7ec18f435417ea3b96d3b9c71ed2a6d5057
Moon <642912728@qq.com>
2022-10-28 00:18:26 +0800

    hot-fix git commit2

--- src/src1/GitTest.java
+++ src/src1/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("所以需要运动一下");
         System.out.println("于是，我叫麻冬丽笨B");
         System.out.println("所以她和我扭打起来");
+        System.out.println("从而，达到了运动的目的");
     }
 }
