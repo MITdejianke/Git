d6f6ffc803a8eb0b646502262a95b09f7c84c44b
Moon <642912728@qq.com>
2022-10-28 00:47:33 +0800

    master git commit

--- src/src1/GitTest.java
+++ src/src1/GitTest.java
@@ -2,11 +2,11 @@ package src1;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("今晚吃的有点撑");
-        System.out.println("所以需要运动一下");
-        System.out.println("于是，我叫麻冬丽笨B");
-        System.out.println("所以她和我扭打起来");
-        System.out.println("从而，达到了运动的目的");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
         System.out.println("我看你冲不冲突");
     }
 }
